6988e9f520ccf96117e4ae17ee51872eba374715
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-04-28 01:46:51 +0300

    authentication

---
+++ IdentityExample/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Data
+{
+    //IdentityDbcontext contains all the tables
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
